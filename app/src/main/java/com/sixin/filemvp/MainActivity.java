76ea982d5a2fab12e7b9922b6566128b0fff6de6
CUTYlife <2055305346@qq.com>
2018-12-19 10:51:15 +0800

    mvp架构模式：准备UI库

---
+++ app/src/main/java/com/sixin/filemvp/MainActivity.java
@@ -0,0 +1,31 @@
+package com.sixin.filemvp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Unbinder mUnbinder;
+
+    @BindView(R.id.rlv_file)
+    RecyclerView mRlvFile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mUnbinder = ButterKnife.bind(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mUnbinder.unbind();
+    }
+}
