3b0b4a0afe3366ac42aacfc4a707142fdaa3fa12
CUTYlife <2055305346@qq.com>
2018-12-19 14:53:47 +0800

    mvp架构模式：添加空试图页面

--- app/src/main/java/com/sixin/filemvp/FileContract.java
+++ app/src/main/java/com/sixin/filemvp/FileContract.java
@@ -3,7 +3,13 @@ package com.sixin.filemvp;
 public interface FileContract {
 
     interface View extends BaseView<Preseneter> {
+        void showLoading();
 
+        void showEmpty();
+
+        void showError();
+
+        void showContent();
     }
 
 
--- app/src/main/java/com/sixin/filemvp/MainActivity.java
+++ app/src/main/java/com/sixin/filemvp/MainActivity.java
@@ -3,6 +3,9 @@ package com.sixin.filemvp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+
+import com.classic.common.MultipleStatusView;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
@@ -13,16 +16,21 @@ public class MainActivity extends AppCompatActivity {
     //TODO 测试修改View，而不影响model的说法
     //TODO 使用mvc写一套
     //TODO 需求变更：文件分类显示
+    //TODO 集成findBugs插件
     private Unbinder mUnbinder;
 
     @BindView(R.id.rlv_file)
     RecyclerView mRlvFile;
+
+    @BindView(R.id.msv_file)
+    MultipleStatusView mMsvFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mUnbinder = ButterKnife.bind(this);
+        mMsvFile.showLoading();
     }
 
     @Override
